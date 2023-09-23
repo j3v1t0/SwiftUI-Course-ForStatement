5a7fe05ac876541378ecdaaa1ae4e577c4f3b02e
Alfonso Almonte <alfonsoalmonte@gmail.com>
2023-09-23 11:47:43 -0400

    For Statement

---
+++ ForStatement.playground/Contents.swift
@@ -0,0 +1,18 @@
+import PlaygroundSupport
+
+let number = 10
+for i in 1...number{
+    print("The number repeats \(i)")
+}
+
+let string = "ABCDEFG"
+
+for letters in string {
+    print(letters)
+}
+
+let names = ["María", "Luisa","Pedro","Mario"]
+
+for name in names{
+    print(name)
+}
